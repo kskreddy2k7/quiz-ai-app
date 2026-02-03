8caedbd321e4b0801b742585e68ac4ef0ab5f736
kskreddy2k7 <kskreddy2k7@gmail.com>
2026-02-03 11:00:47 +0530

    Adjust quiz generation limit and sentence filtering

--- quiz_generator.py
+++ quiz_generator.py
@@ -1,9 +1,9 @@
 import random
 import re
 
-def generate_quiz(text, limit=10):
+def generate_quiz(text, limit=5):
     sentences = re.split(r'[.!?]', text)
-    sentences = [s.strip() for s in sentences if len(s.split()) > 8]
+    sentences = [s.strip() for s in sentences if len(s.split()) > 6]
 
     keywords = list(set(re.findall(r'\b[A-Z][a-z]{3,}\b', text)))
     fallback = ["Python", "Java", "C++", "JavaScript"]
@@ -11,12 +11,12 @@ def generate_quiz(text, limit=10):
     quiz = []
 
     for s in sentences[:limit]:
-        words = s.split()
-        answer = words[0]
-
+        answer = s.split()[0]
         options = random.sample(keywords + fallback, 4)
+
         if answer not in options:
             options[0] = answer
+
         random.shuffle(options)
 
         quiz.append({
