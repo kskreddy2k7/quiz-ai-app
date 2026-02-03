2f5fa8077ee747aaf572db894793b5709012ef8c
kskreddy2k7 <kskreddy2k7@gmail.com>
2026-02-03 14:13:40 +0530

    Update quiz_generator.py

--- quiz_generator.py
+++ quiz_generator.py
@@ -5,14 +5,12 @@ def generate_quiz(text, limit=5):
     sentences = re.split(r'[.!?]', text)
     sentences = [s.strip() for s in sentences if len(s.split()) > 6]
 
-    keywords = list(set(re.findall(r'\b[A-Z][a-z]{3,}\b', text)))
     fallback = ["Python", "Java", "C++", "JavaScript"]
-
     quiz = []
 
     for s in sentences[:limit]:
         answer = s.split()[0]
-        options = random.sample(keywords + fallback, 4)
+        options = random.sample(fallback, 4)
 
         if answer not in options:
             options[0] = answer
